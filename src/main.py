cc57b70e99140776b375a14192c01b48488b7c09
Schuyler Good <58613697+SchuylerGood@users.noreply.github.com>
2022-10-26 20:59:31 -0400

    Finished draft for the pieces classes.

--- src/main.py
+++ src/main.py
@@ -3,8 +3,13 @@ import board as b
 
 
 def main():
-    board = b.create_board(4)
-    b.print_board(board)
+    list_of_pieces = pieces.generate_n_pieces(n)
+    n = 8
+    board = b.board(n)
+    
+    for i in range(n):
+        board.set((0,i), list_of_pieces[i])
 
+    board.print()
 
 main()
--- src/pieces.py
+++ src/pieces.py
@@ -1,38 +1,67 @@
 # This is the OOP structure for the pieces
 
+from random import randint
 
 class piece:
-    def __init__(self, name):
-        self.name = name
-
-    def placePiece(self, x, y):
-        print("goofy ahh")
+    def __init__(self):
+        self
 
 
-class queen(piece):
-    def __init__(self, horizontal_conditions, vertical_conditions, diagonal_conditions, *args, **kwargs):
-        super(queen, self).__init__(*args, **kwargs)
-        self.horizontal_conditions = horizontal_conditions
-        self.vertical_conditions = vertical_conditions
+
+class bishop(piece):
+    def __init__(self, diagonal_conditions):
         self.diagonal_conditions = diagonal_conditions
 
-class bishop(queen):
-    def __init__(self, *args, **kwargs):
-        super(bishop, self).__init__(*args, **kwargs)
+    def __str__(self):
+        return "B"
+
+class rook(piece):
+    def __init__(self, horizontal_conditions, vertical_conditions):
+        self.horizontal_conditions = horizontal_conditions
+        self.vertical_conditions = vertical_conditions
+
+    def __str__(self):
+        return "R"
 
 
-class rook(queen):
-    def __init__(self, *args, **kwargs):
-        super(rook, self).__init__(*args, **kwargs)
+class queen(rook):
+    def __init__(self, horizontal_conditions, vertical_conditions, diagonal_conditions):
+        super().__init__(horizontal_conditions, vertical_conditions)
+        self.diagonal_conditions = diagonal_conditions
+
+    def __str__(self):
+        return "Q"
 
 class knight(piece):
     def __init__(self, knight_conditions):
         self.knight_conditions  = knight_conditions
 
+    def __str__(self):
+        return "H"
+
 class king(piece):
     def __init__(self, king_conditions):
         self.king_conditions = king_conditions
 
+    def __str__(self):
+        return "K"
+
+
+
+def generate_n_pieces(n):
+    list_of_pieces = []
+    dictionary_of_pieces = {
+        0:queen(True,True,True),
+        1:bishop(True),
+        2:rook(True,True),
+        3:knight(True),
+        4:king(True)
+    }
+    for i in range(n):
+        random_num = randint(0,4)
+        list_of_pieces.append(dictionary_of_pieces[random_num])
+    
+    return list_of_pieces
 
 
 

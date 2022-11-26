51443b796f869c43c39c67a2282f37c59c9edbf6
aksdes12111 <aksdes1211@gmail.com>
2022-11-26 16:17:33 -0500

    comments for recursion function

--- src/main.py
+++ src/main.py
@@ -33,24 +33,31 @@ def remove_piece(b, piece):
     positions.remove(pos)
 
 
-def solution(b, pieces, n):
-    if (n == b.get_size):
+# Explaination for this function as it is the most important function in the whole implementation
+def solution(b, pieces, n, solution):
+    if (n == b.get_size):  # this is base case at this position we will have the solution
+        # appending the solution as we have multiple solution and we find them all
+        solution.append(b.get_position())
         return
     for i in range(b.get_size()):
+        # get the piece at the nth location to be places in the nth row
         piece = pieces[n]
-        piece.i = n
-        piece.j = i
-        if(is_safe(b, piece)):
+        piece.i = n  # assigning the row number to i
+        piece.j = i  # assigning th column number to j
+        if(is_safe(b, piece)):  # this function will check if placement of the piece is safe of not and will also place the piece
             # insert_place(b, piece)
+            # then we call the recursion function for next row,
             solution(b, pieces, n+1)
+            # while returning it will remove the piece from the board
             remove_piece(b, piece)
         else:
+            # as is safe places the pices along with checking of the placment safety hence have to remove the piece if it not not a safe position
             remove_piece(b, piece)
 
 
 def main():
-    list_of_pieces = pieces.generate_n_pieces(n)
     n = 8
+    list_of_pieces = pieces.generate_n_pieces(n)
     board = b.board(n)
 
     for i in range(n):
